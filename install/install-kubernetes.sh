9efaa3a9ce428597815f8446e5df790f43205e41
danielitogomez <daniel.gomez.amaran@gmail.com>
2020-08-29 16:33:37 -0400

    fix comment

--- install/install-kubernetes.sh
+++ install/install-kubernetes.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #Only on Master Node
 
-#Install packges
+#Install packages
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
 $(lsb_release -cs) \
