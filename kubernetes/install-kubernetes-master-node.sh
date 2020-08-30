dd4de95a470da51eabd56de9c3a5218f5c538eb6
danielitogomez <daniel.gomez.amaran@gmail.com>
2020-08-30 01:45:48 -0400

    add new scripts

---
+++ kubernetes/install-kubernetes-master-node.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#Only on Master Node
+
+#Install packages
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+$(lsb_release -cs) \
+stable"
+sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+cat << EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
+deb https://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+
+# Install Docker and kube{let,ctl,adm} version 1.17.8
+sudo apt-get update -y && sudo apt-get install -y docker-ce=5:19.03.12~3-0~ubuntu-bionic kubelet=1.17.8-00 kubeadm=1.17.8-00 kubectl=1.17.8-00 && sudo apt-mark hold docker-ce kubelet kubeadm kubectl
+
+#Config Master Node
+echo "net.bridge.bridge-nf-call-iptables=1" | sudo tee -a /etc/sysctl.conf
+sudo sysctl -p
+sudo kubeadm init --pod-network-cidr=10.244.0.0/16
+
+mkdir -p $HOME/.kube && sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config && sudo chown $(id -u):$(id -g) $HOME/.kube/config && kubectl apply -f https://docs.projectcalico.org/v3.14/manifests/calico.yaml
---
+++ kubernetes/update-kubernetes-version-working-node.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#Working Node
+
+showHelp()
+{
+    echo "Hi, please use flag -u and the version that you want to update"
+    echo "For example: ./update-kubernetes-version-working-node -u 1.18.5-00"
+}
+
+while getopts "h:u:" OPTION; do
+        case $OPTION in
+                u)
+                        update=$2
+                        ;;
+                h)
+                        showHelp
+                        exit 4
+                        ;;
+                ?)
+                        showHelp
+                        exit 3
+                        ;;
+        esac
+done
+
+
+sudo apt-mark hold kubectl && sudo apt install -y "kubelet=$update" && sudo apt-mark hold kubelet
+kubelet --version
