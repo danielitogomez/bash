72b4b0d30e9491dee8f5a8f49b65ad24d2987986
danielitogomez <daniel.gomez.amaran@gmail.com>
2020-08-30 01:41:40 -0400

    add new script for update kubernetes working node and fix while case

--- kubernetes/update-kubernetes-version.sh
+++ kubernetes/update-kubernetes-version.sh
@@ -1,25 +1,30 @@
 #!/bin/bash
-  
-while test $# -gt 0; do
-           case "$1" in
-                -u)
-                    shift
-                    update=$1
-                    shift
-                    ;;
-                -h)
-                    shift
-                    update=$1
-                    echo "============Help============="
-                    echo "Use flag -u for input the version of kube{adm,ctl,let}"
-                    exit 1
-                    ;;
-                 *)
-                    echo "Not a recognized flag! please use flag -h for view the help"
-                    exit 1
-                    ;;
-          esac
-  done
+#Master Node
+
+showHelp()
+{
+        echo -e "\033[31m           0000\033[0m_____________0000________0000000000000000__000000000000000000+\n\033[31m         00000000\033[0m_________00000000______000000000000000__0000000000000000000+\n\033[31m        000\033[0m____000_______000____000_____000_______0000__00______0+\n\033[31m       000\033[0m______000_____000______000_____________0000___00______0+\n\033[31m      0000\033[0m______0000___0000______0000___________0000_____0_____0+\n\033[31m      0000\033[0m______0000___0000______0000__________0000___________0+\n\033[31m      0000\033[0m______0000___0000______0000_________000___0000000000+\n\033[31m      0000\033[0m______0000___0000______0000________0000+\n\033[31m       000\033[0m______000_____000______000________0000+\n\033[31m        000\033[0m____000_______000____000_______00000+\n\033[31m         00000000\033[0m_________00000000_______0000000+\n\033[31m           0000\033[0m_____________0000________000000000;"
+echo "########################################################################"
+echo "Hi, please use flag -u and the version that you want to update"
+echo "For example: ./update-kubernetes-version.sh -u 1.18.5-00"
+}
+
+
+while getopts "h:u:" OPTION; do
+        case $OPTION in
+                u)
+                        update=$2
+                        ;;
+                h)
+                        showHelp
+                        exit 4
+                        ;;
+                ?)
+                        showHelp
+                        exit 3
+                        ;;
+        esac
+done
 
 #Update kubeadm
 kubeadm version
@@ -28,13 +33,13 @@ sudo apt-mark unhold kubeadm kubelet && sudo apt install -y "kubeadm=$update"
 if [ $? -eq 0 ]; then
     echo "=========Go to hold kubeadm============"
                 sudo apt-mark hold kubeadm
-    echo "======================================="      
         if [ $? -eq 0 ]; then
-            sudo kubeadm upgrade plan
-            echo "===============Go to upgrade apply================"
+                sudo kubeadm upgrade plan
+            echo "==============Go to upgrade apply Kubeadm=============="
+            
             #Version Definition
             var_version=$(echo $update | awk '{print substr($0, 1, length($0)-3)}')
-            sudo kubeadm upgrade apply v$var_version -y
+                        sudo kubeadm upgrade apply v$var_version -y   
                 if [ $? -eq 0 ]; then
                     echo "Apply kubeadm finish Good"
                     kubeadm version
@@ -56,7 +61,6 @@ sudo apt-mark unhold kubectl && sudo apt install -y "kubectl=$update"
 if [ $? -eq 0 ]; then
     echo "=========Go to hold kubectl============"
                 sudo apt-mark hold kubectl
-    echo "======================================="
         if [ $? -eq 0 ]; then
             echo "Update kubectl finish Good"
         else
@@ -74,7 +78,6 @@ sudo apt install -y "kubelet=$update"
 if [ $? -eq 0 ]; then
     echo "=========Go to hold kubelet============"
                 sudo apt-mark hold kubelet
-    echo "======================================="
         if [ $? -eq 0 ]; then
             echo "Update kubelet finish Good"
         else
@@ -85,9 +88,3 @@ else
     echo "Can't full update kubelet"
     exit 1
 fi
-
-echo "========Present Versions========="
-kubectl version --short
-kubeadm version
-kubelet --version
-echo "================================="
