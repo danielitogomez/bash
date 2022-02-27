8e6d0476dab858efd863994dfd2dce3acf498c61
danielitogomez <daniel.gomez.amaran@gmail.com>
2022-02-27 18:19:41 -0300

    feat: new script to create dir on remote server

---
+++ sysadmin-tools/remote-mkdir/README.md
@@ -0,0 +1,14 @@
+## Bash script for create directory to remote server
+Creating a directory to remote server
+
+## Requirements
+To have [bash shell](https://www.gnu.org/software/bash/). All kinds of Linux distros should be able to run it.<br/>
+To have [Netcat tool](http://netcat.sourceforge.net/). To test that you are able to reach port 22 on the remote server.
+To have a pair ssh private/public key. Review this [docu](https://www.ssh.com/academy/ssh/public-key-authentication).
+
+## Steps
+```
+* git clone https://github.com/danielitogomez/bash.git
+* cd $PWD/bash/sysadmin-tools/remote-mkdir
+* ./remote-mkdir.sh
+```
---
+++ sysadmin-tools/remote-mkdir/remote-mkdir.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# Uncomment this line to run the script in debug mode
+#set -x 
+
+# Declare variables
+
+SSH_PUB_KEY="$HOME/.ssh/id_rsa_remote_server"
+USER="cloud_user"
+SERVER="34.218.222.169"
+CMD="mkdir /home/cloud_user/$(date +%Y-%m-%d)$(date +%H%M%S)"
+STATUS=$?
+PORT=22
+DATE=$(date +%Y-%m-%d)
+TIME=$(date +%H%M%S)
+
+# Functions
+
+server_conection_status_port_22 () {
+	echo "####Testing connection to remote server $SERVER...####"
+if [ "$(nc -z -w3 $SERVER $PORT; echo $?)" -eq 0 ]; then
+	echo "At $DATE $TIME The server $SERVER on port $PORT: Connected"
+else
+	echo "At $DATE $TIME The server $SERVER on port $PORT : No connection"
+	exit 1
+fi
+}
+
+exit_status () {
+if [ $STATUS = 0 ]; then
+	echo "Creating directory..."
+else
+	echo "Cannot create directory..."
+	exit 1
+fi
+}
+
+function_remote_server () {
+echo "Connecting to remote server...";
+	if [ "$(ssh -i $SSH_PUB_KEY $USER@$SERVER "$CMD"; echo $?)" = "0" ]; then
+		echo "Connecting success..."
+		exit_status
+	else
+		echo "Unsucess conection..."
+		exit 1
+	fi
+}
+
+# Functions call
+
+server_conection_status_port_22 && function_remote_server
+
+
+
+
