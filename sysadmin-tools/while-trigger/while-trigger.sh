bedcbd398be2b845209fb8623f4cfc6588831970
danielitogomez <daniel.gomez.amaran@gmail.com>
2022-05-10 00:16:16 -0400

    feat: add new script to while trigger

---
+++ sysadmin-tools/while-trigger/while-trigger.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Object white_rabbit
+
+OBJ_WHITE_RABBIT="white_rabbit" && mkdir $OBJ_WHITE_RABBIT
+
+# Trigger directory
+
+MY_TRIGGER="my_trigger" && mkdir $MY_TRIGGER
+
+# Function testing my trigger
+
+function test_my_trigger () {
+if [ ! -d "$HOME/$MY_TRIGGER" ]; then
+    echo "Doing my stuff and break loop"
+    rm -rf $OBJ_WHITE_RABBIT
+    break
+else
+    echo "Still exiting $MY_TRIGGER"
+fi
+}
+
+# Function loop delete trigger if eq set time
+
+function loop_delete_my_trigger () {
+while [ "$(date '+%Y/%m/%d %H:%M:%S' | awk '{print $2}')" == 22:59:15 ]; do 
+  echo "Deleting $MY_TRIGGER"
+  rm -rf $MY_TRIGGER
+  break
+done
+}
+
+# Function main loop
+
+function loop_my_trigger () {
+while true; 
+    do 
+    loop_delete_my_trigger;
+    test_my_trigger;
+done
+}
+
+# Call loop function my trigger
+
+loop_my_trigger
