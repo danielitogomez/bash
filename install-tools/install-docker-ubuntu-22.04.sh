1b8b2a5fb4b78b91e82d7fb090c15b386a42343e
danielitogomez <daniel.gomez.amaran@gmail.com>
2022-12-15 14:29:29 +0100

    feat: add new script to install docker on Ubuntu 22.04

---
+++ install-tools/install-docker-ubuntu-22.04.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce
+# Linux post-install
+sudo groupadd docker
+sudo usermod -aG docker $USER
+sudo systemctl enable docker
+sudo chmod 666 /var/run/docker.sock
